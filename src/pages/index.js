128674f3be827342e0728fcc4dbf3b89601ff62b
Elisa <elisacanyelles@gmail.com>
2022-01-18 19:03:55 +0100

    created directories and exported them as a name

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import About from './AboutPage'
+import Error from './ErrorPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+
+export {
+ Home, Products, SingleProduct, About, Error, Cart, Checkout, PrivateRoute
+}
