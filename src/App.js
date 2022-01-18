45f326e79be5298b3db5fa752bbcda4b0251e171
Elisa <elisacanyelles@gmail.com>
2022-01-18 19:02:33 +0100

    set up the routes and added fix components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import { Navbar, Sidebar, Footer } from './components'  
+import { Home, Products, SingleProduct, About, Error, Cart, Checkout, PrivateRoute } from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+  <Router> 
+    <Navbar/>
+    <Sidebar/>
+  <Switch>
+   <Route exact path="/">
+     <Home/>
+   </Route>
+   <Route exact path="/about">
+    <About />
+    </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products/>
+        </Route>
+         <Route exact path="/products/:id" children={<SingleProduct/>}/> {/*will redirect to the id of SingleProduct */}
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+         <Route exact path="/*">  {/** is gonna match any path */}
+          <Error />
+        </Route>
+  </Switch>
+  <Footer/>
+  </Router>
+  )
 }
 
 export default App
